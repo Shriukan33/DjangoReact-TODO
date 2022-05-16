f604af06def23f519e8a16aa4f01675d6e640d39
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-05-16 22:20:10 +0200

    Add TodoView
    This view allows CRUD operations on todo items.

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
-# Create your views here.
+
+# The viewsets base class provides the implementation for CRUD operations
+# by default.
+class TodoView(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
